7392f66d2c778a697d04d88a1f784144e4c14487
Soundwave0 <konradkosuge@gmail.com>
2025-11-08 18:11:02 +0100

    gui implement

---
+++ build/CMakeFiles/TheDenigifier_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TheDenigifier_autogen"
+  "TheDenigifier_autogen/mocs_compilation.cpp"
+  "TheDenigifier_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/TheDenigifier_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
